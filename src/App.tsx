a35eacc09d0d83158ad69d8ce3c4af61dd6bcf5e
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2021-12-16 17:44:30 +0300

    confession slice $6

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import HeaderPanel from './components/layout/HeaderPanel';
 import SidePanel from './components/layout/SidePanel';
+import ConfessionsList from './features/confession/ConfessionsList';
 
 const App = () => {
   return (
@@ -9,10 +10,14 @@ const App = () => {
       <HeaderPanel />
       <Grid>
         <Grid.Row columns={2}>
-          <Grid.Column width={6}>
+          <Grid.Column width={5}>
             <SidePanel />
           </Grid.Column>
-          <Grid.Column width={10}>{/* Main content */}</Grid.Column>
+          <Grid.Column width={10}>
+            <div style={{ position: 'absolute', top: 80, left: 30 }}>
+              <ConfessionsList />
+            </div>
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     </div>
---
+++ src/features/confession/Confession.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { pickTagColor } from '../../utils/functions';
+import moment from 'moment';
+import maleAvatar from '../../images/male_avatar.png';
+import femaleAvatar from '../../images/female_avatar.png';
+import { Segment, Comment, Divider, Label, Icon } from 'semantic-ui-react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { setCurrentCategory } from './confessionSlice';
+
+const Confession = ({ confession }: any) => {
+  const dispatch = useAppDispatch();
+  const { currentUser }: any = useAppSelector((state) => state.auth);
+
+  const getUserAvatar = () => {
+    const gender = confession.user.gender;
+
+    let avatar = gender === 'female' ? femaleAvatar : maleAvatar;
+    if (confession.shareAs === 'user') {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      avatar = confession.user.photoURL ? confession.user.photoURL : avatar;
+    }
+    return <Comment.Avatar src={avatar} style={{ marginRight: '1em' }} />;
+  };
+  const timeFromNow = (timestamp: any) => moment(timestamp).fromNow();
+
+  return (
+    <Segment loading={!confession} style={{ width: '740px' }}>
+      <Comment.Group style={{ marginLeft: '30px !important' }}>
+        <Comment>
+          {getUserAvatar()}
+          <Comment.Content>
+            {confession.shareAs === 'user' ? (
+              <Comment.Author as='a'>{confession.user.username}</Comment.Author>
+            ) : (
+              <Comment.Author as='a'>Anonim</Comment.Author>
+            )}
+            <Comment.Metadata>
+              <span>{timeFromNow(confession.timestamp)}</span>
+            </Comment.Metadata>
+            <span style={{ position: 'absolute', right: '-40px', top: '-7px' }}>
+              <Icon
+                name={
+                  confession.favorites[currentUser.uid] === 1
+                    ? 'heart'
+                    : 'heart outline'
+                }
+                size='large'
+              />
+            </span>
+            <Comment.Text>{confession.content}</Comment.Text>
+            {confession.tags.map((prop: any, key: any) => (
+              <Label
+                key={key}
+                as='a'
+                basic
+                size='tiny'
+                color={pickTagColor(prop) as any}
+                onClick={(event, data) => {
+                  dispatch(setCurrentCategory(prop));
+                }}
+              >
+                <span>{prop}</span>
+              </Label>
+            ))}
+
+            <Divider />
+
+            <Comment.Actions>
+              <Comment.Action>
+                <Icon
+                  name={
+                    confession.feelings[currentUser.uid] === 1
+                      ? 'thumbs up'
+                      : 'thumbs up outline'
+                  }
+                />
+                {confession.numberOfLikes}
+              </Comment.Action>
+              <Comment.Action>
+                <Icon
+                  name={
+                    confession.feelings[currentUser.uid] === -1
+                      ? 'thumbs up'
+                      : 'thumbs up outline'
+                  }
+                />
+                {confession.numberOfDislikes}
+              </Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      </Comment.Group>
+    </Segment>
+  );
+};
+
+export default Confession;
